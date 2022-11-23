3d51d7fb3c0187c162eaedab24d90dab1a439d20
felwoy <91830402+felwoy@users.noreply.github.com>
2022-11-23 22:20:13 +0100

    Update test_basket.py

--- tests/test_basket.py
+++ tests/test_basket.py
@@ -6,10 +6,22 @@ def test_basket():
     assert solve([2, 2, 2, 1, 1], [0, 0.05, 0.1, 0.2, 0.25], 8) == 51.2, \
             "basic logic/math failed"
 
+    
+# test that no books get lost by building sets, could do similar tests for all functions
+def test_setfinding():
+    basket = [3, 4, 2, 1, 1]
+    basket_total = 11
+    ret = find_big_sets(basket)
+    ret_total  = 0
+    for i in range(len(ret)):
+        ret_total += (len(ret) - i) * ret[i]
+    assert ret_total == basket_total
 #### place for more tests, noticed there is a lot more in testing to do
 
 
 
 if __name__ == '__main__':
     test_basket()
-    print("test passed")
+    print("example test passed")
+    test_setfinding()
+    print("set building test passed")
