5f4b62e46d1207626020bc58eafe0730833855e8
felwoy <fw0415@googlemail.com>
2022-11-23 21:09:57 +0100

    branch commit

--- basket/basket.py
+++ basket/basket.py
@@ -26,9 +26,38 @@ def calculate_price(basket, discounts): # calculates price from given set_basket
         ret += basket[i] * (1-discounts[-(i+1)]) * (len(basket)-i)
     return ret
 
+def find_prio_basket(discounts):
+    max = 0
+    max_index = -1
+    for i in range(1, len(discounts)):
+        if discounts[i] - discounts[i-1] > max:
+            max = discounts[i] - discounts[i-1]
+            max_index = i+1
+    assert max_index > 0, \
+           "failed to get a suitable max_index"
+    return max_index
+
+def prio_sets(prio, basket):
+    if prio == len(basket):
+        return basket
+    if basket[-prio-1] != 0 and basket[-prio+1] !=0:
+        if basket[-prio+1] < basket[-prio-1]:
+            basket[-prio] += 2 * basket[-prio+1]
+            basket[-prio-1] -= basket[-prio+1]
+            basket[-prio+1] = 0
+        else:
+            basket[-prio] += 2 * basket[-prio-1]
+            basket[-prio+1] -= basket[-prio-1]
+            basket[-prio-1] = 0
+    return basket
+
+
+
+
 def solve(basket, discounts, base_price): #solves whole problem using 3 functions above
     basket = find_big_sets(basket)
-    basket = prio_4sets(basket)
+    basket = prio_sets(find_prio_basket(discounts), basket)
+    print(basket)
     return calculate_price(basket, discounts) * base_price
 
 if __name__ == "__main__":
@@ -37,3 +66,4 @@ if __name__ == "__main__":
     base_price = 8
 
     print(solve(basket, discounts, base_price))
+    print(find_prio_basket(discounts))
