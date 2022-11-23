6ffb84d145aa15eb0354ba9822b2c2243a0af6e5
felwoy <fw0415@googlemail.com>
2022-11-23 22:54:53 +0100

    branch commit

--- tests/test_basket.py
+++ tests/test_basket.py
@@ -1,6 +1,6 @@
 from basket.basket import *
 
-# basic test to prove the given example is fullfilled, since that's the only verified solution
+# basic test to prove the given example is fulfilled, since that's the only verified solution
 # could have been done via unittest as well
 def test_basket():
     assert solve([2, 2, 2, 1, 1], [0, 0.05, 0.1, 0.2, 0.25], 8) == 51.2, \
@@ -12,11 +12,11 @@ def test_setfinding():
     basket = [3, 4, 2, 1, 1]
     basket_total = 11
     ret = find_big_sets(basket)
-    ret_total  = 0
+    ret_total = 0
     for i in range(len(ret)):
         ret_total += (len(ret) - i) * ret[i]
     assert ret_total == basket_total, \
-            "lost or found books during set_building"
+            "lost or found books during set building"
 #### place for more tests, noticed there is a lot more in testing to do
 
 
