cba8726bd7b51942bf3193833537c62f489b6ba0
felwoy <fw0415@googlemail.com>
2022-11-23 21:42:59 +0100

    branch commit

--- basket/basket.py
+++ basket/basket.py
@@ -6,17 +6,6 @@ def find_big_sets(basket): #builds sets, from big sets to small ones
         count += basket[i]
     return basket
 
-def prio_4sets(basket): #only works for THIS problem, combines sets of 3 and 5 to 2 sets of 4
-    if basket[2] != 0 and basket[0] != 0:
-        if basket[2] < basket[0]:
-            basket[1] += 2*basket[2]
-            basket[0] -= basket[2]
-            basket[2] = 0
-        else:
-            basket[1] += 2*basket[0]
-            basket[2] -= basket[0]
-            basket[0] = 0
-    return basket
 
 def calculate_price(basket, discounts): # calculates price from given set_basket and discounts
     assert len(basket) == len(discounts), \
@@ -26,7 +15,8 @@ def calculate_price(basket, discounts): # calculates price from given set_basket
         ret += basket[i] * (1-discounts[-(i+1)]) * (len(basket)-i)
     return ret
 
-def find_prio_basket(discounts):
+
+def find_prio_basket(discounts): #find set that give best discounts
     max = 0
     max_index = -1
     for i in range(1, len(discounts)):
@@ -34,13 +24,14 @@ def find_prio_basket(discounts):
             max = discounts[i] - discounts[i-1]
             max_index = i+1
     assert max_index > 0, \
-           "failed to get a suitable max_index"
+           "failed to get a suitable max_index" # 0 not suitable
     return max_index
 
-def prio_sets(prio, basket):
-    if prio == len(basket):
+
+def prio_sets(prio, basket): #transforms oen lower and oen higher in two prio sets
+    if prio >= len(basket) or prio <= 0: #can't prio that, could argue about asserting this
         return basket
-    if basket[-prio-1] != 0 and basket[-prio+1] !=0:
+    if basket[-prio-1] != 0 and basket[-prio+1] !=0: #well, I regret how i ordered discounts
         if basket[-prio+1] < basket[-prio-1]:
             basket[-prio] += 2 * basket[-prio+1]
             basket[-prio-1] -= basket[-prio+1]
@@ -52,13 +43,12 @@ def prio_sets(prio, basket):
     return basket
 
 
-
-
-def solve(basket, discounts, base_price): #solves whole problem using 3 functions above
+def solve(basket, discounts, base_price): #solves whole problem using functions above
     basket = find_big_sets(basket)
     basket = prio_sets(find_prio_basket(discounts), basket)
-    print(basket)
     return calculate_price(basket, discounts) * base_price
+
+
 
 if __name__ == "__main__":
     basket = [2, 2, 2, 1, 1]
@@ -66,4 +56,3 @@ if __name__ == "__main__":
     base_price = 8
 
     print(solve(basket, discounts, base_price))
-    print(find_prio_basket(discounts))
