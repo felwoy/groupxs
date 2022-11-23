c4ef780c853fe5f7da13623420d2ce14e42295f2
felwoy <91830402+felwoy@users.noreply.github.com>
2022-11-23 20:10:57 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,11 @@
 from basket.basket import *
 
+# basic test to prove the given example is fullfilled, since that's the only verified solution
 def test_basket():
     assert solve([2, 2, 2, 1, 1], [0, 0.05, 0.1, 0.2, 0.25], 8) == 51.2, \
             "basic logic/math failed"
 
-#### place for more tests, noticed that I should probably know more about testing
+#### place for more tests, noticed there is a lot more in testing to do
 
 
 
