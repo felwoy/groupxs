f1c4498f60672ff3717de370cb0e363907f1daea
felwoy <fw0415@googlemail.com>
2022-11-23 22:24:02 +0100

    branch commit

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,24 @@
 from basket.basket import * #should import solve only here
 
-# basic test to prove the given example is fullfilled, since that's the only verified solution
+# basic test to prove the given example is fullfilled, since that's the only truely verified solution
 def test_basket():
     assert solve([2, 2, 2, 1, 1], [0, 0.05, 0.1, 0.2, 0.25], 8) == 51.2, \
             "basic logic/math failed"
 
-#### place for more tests, noticed there is a lot more in testing to do
+# test that no books get lost by building sets, could do similar tests for all functions
+def test_setfinding():
+    basket = [3, 4, 2, 1, 1]
+    basket_total = 11
+    ret = find_big_sets(basket)
+    ret_total  = 0
+    for i in range(len(ret)):
+        ret_total += (len(ret) - i) * ret[i]
+    assert ret_total == basket_total
 
+#### place for more tests, noticed there is a lot more in testing to do
 
 
 if __name__ == '__main__':
     test_basket()
+    test_setfinding()
     print("test passed")
